5608903373ff396e6a93efa8c7be060b2fd6c449
VPresich <111353667+VPresich@users.noreply.github.com>
2023-11-28 13:54:37 +0000

    Deploying to gh-pages from @ VPresich/goit-js-homework-12@8ac1c90c4baebc812395b7afca79a0645ced4d11 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,9 +1,9 @@
-import{g as i,B as l,a as h,c as u}from"./assets/get-images-18c7ea61.js";/* empty css                      */import{i as g,S as f}from"./assets/vendor-9310f15c.js";const s="/goit-js-homework-12/assets/icons-5e8cbe24.svg",m="/goit-js-homework-12/assets/bi-x-octagon-73f29380.svg";function b(e){g.show({message:e,messageColor:"#fafafb",messageSize:"16px",backgroundColor:"#fc5a5a",position:"topRight",closeOnEscape:!0,close:!0,icon:"Icomoon",iconUrl:`${m}`,iconColor:"#fafafb"})}const o=document.querySelector(".gallery"),t=document.querySelector(".border-loader"),a=document.querySelector(".search-form");a.addEventListener("submit",d);let c=new f(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,navText:[`<svg class="lightbox-button-icon-prev" width="24" height="24" aria-label="icon arrow left">
-          <use href="${s}#icon-chevron-left"></use>
+import{g as l,c as i,B as h,a as u,b as g}from"./assets/create-msg-a088259a.js";/* empty css                      */import{S as m}from"./assets/vendor-9310f15c.js";const t="/goit-js-homework-12/assets/icons-5e8cbe24.svg",r=document.querySelector(".gallery"),s=document.querySelector(".border-loader"),a=document.querySelector(".search-form");a.addEventListener("submit",b);let n=new m(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,navText:[`<svg class="lightbox-button-icon-prev" width="24" height="24" aria-label="icon arrow left">
+          <use href="${t}#icon-chevron-left"></use>
         </svg>`,`<svg class="lightbox-button-icon-next" width="24" height="24" aria-label="icon arrow right">
-          <use href="${s}#icon-chevron-right"></use>
+          <use href="${t}#icon-chevron-right"></use>
         </svg>`],close:!0,closeText:`
         <svg class="lightbox-close-icon" width="16" height="16">
-          <use href="${s}#close-form"></use>
-        </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});function d(e){e.preventDefault(),t.style.display="block";const n=e.currentTarget.search.value.trim();i(n).then(r=>v(r)).catch(r=>p(r))}function p(e){a.search.value="",t.style.display="none",b(e),o.style.backgroundColor=l,o.innerHTML="",c.refresh()}function v(e){a.search.value="",t.style.display="none",o.style.backgroundColor=h,u(e.hits,o),c.refresh()}
+          <use href="${t}#close-form"></use>
+        </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});function b(e){e.preventDefault(),s.style.display="block";const c=e.currentTarget.search.value.trim();l(c).then(o=>f(o)).catch(o=>d(o))}function d(e){a.search.value="",s.style.display="none",i(e),r.style.backgroundColor=h,r.innerHTML="",n.refresh()}function f(e){a.search.value="",s.style.display="none",r.style.backgroundColor=u,g(e.hits,r),n.refresh()}
 //# sourceMappingURL=commonHelpers2.js.map
