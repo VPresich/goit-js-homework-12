024a6dd25d2cf4454a1b8b273ae51d7f05df5dea
VPresich <valya.alexandrova.presich@gmail.com>
2023-11-28 18:35:31 +0100

    async-await

--- src/js/common/fetch-data.js
+++ src/js/common/fetch-data.js
@@ -1,30 +1,22 @@
-function fetchData(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          reject(response.status);
-        }
-        resolve(response.json());
-      })
-      .catch(error => reject(error));
-  });
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  return response.json();
 }
 
 export default fetchData;
 
-// without promise
-// async function fetchData(url, apiKey) {
-//   const options = {
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'X-API-KEY': apiKey,
-//     },
-//   };
-//   return fetch(url).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
+// function fetchData(url) {
+//   return new Promise((resolve, reject) => {
+//     fetch(url)
+//       .then(response => {
+//         if (!response.ok) {
+//           reject(response.status);
+//         }
+//         resolve(response.json());
+//       })
+//       .catch(error => reject(error));
 //   });
 // }
--- src/js/common/get-images.js
+++ src/js/common/get-images.js
@@ -1,7 +1,7 @@
 import fetchData from './fetch-data.js';
 import { API_KEY, API_URL } from '../common/constants.js';
 
-function getImages(strForSearch, pageNumber = 1) {
+async function getImages(strForSearch, pageNumber = 1) {
   const apiParams = {
     key: API_KEY,
     q: encodeURIComponent(strForSearch),
@@ -11,20 +11,48 @@ function getImages(strForSearch, pageNumber = 1) {
     page: pageNumber,
     per_page: 20,
   };
+
   const url = `${API_URL}?${new URLSearchParams(apiParams).toString()}`;
 
-  return new Promise((resolve, reject) => {
-    fetchData(url)
-      .then(data => {
-        if (!data.hits.length) {
-          reject(
-            'Sorry, there are no images matching your search query. Please, try again!'
-          );
-        }
-        resolve(data);
-      })
-      .catch(error => reject(error));
-  });
+  try {
+    const data = await fetchData(url);
+
+    if (!data.hits.length) {
+      return Promise.reject(
+        'Sorry, there are no images matching your search query. Please, try again!'
+      );
+    }
+    return Promise.resolve(data);
+  } catch (error) {
+    console.error('Error:', error);
+    return Promise.reject(error);
+  }
 }
 
 export default getImages;
+
+// function getImages(strForSearch, pageNumber = 1) {
+//   const apiParams = {
+//     key: API_KEY,
+//     q: encodeURIComponent(strForSearch),
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//     page: pageNumber,
+//     per_page: 20,
+//   };
+//   const url = `${API_URL}?${new URLSearchParams(apiParams).toString()}`;
+
+//   return new Promise((resolve, reject) => {
+//     fetchData(url)
+//       .then(data => {
+//         if (!data.hits.length) {
+//           reject(
+//             'Sorry, there are no images matching your search query. Please, try again!'
+//           );
+//         }
+//         resolve(data);
+//       })
+//       .catch(error => reject(error));
+//   });
+// }
